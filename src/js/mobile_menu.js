ae4f26802df9f6974814a7d93c5ce5335d39ccf7
Karn Kamolnavin <karn.lattecandy@gmail.com>
2025-01-30 11:51:53 +0700

    WIP

--- src/js/mobile_menu.js
+++ src/js/mobile_menu.js
@@ -1,7 +1,19 @@
-async function toggleMobileNav() {
+function toggleMobileNav() {
+    const hideMenu = "hideMenu";
+    const showMenu = "showMenu";
     const mobileMenu = document.getElementById("mobileNav");
-    mobileMenu.classList.toggle("hideMenu");
-    mobileMenu.classList.toggle("showMenu");
+    const hamburgerDiv = document.getElementById("hamburger_menu");
+    const hamburgerButton = '<i class="bi bi-list text-white display-3"></i>';
+    const closeButton = '<i class="bi bi-x-lg text-white display-3"></i>';
+    mobileMenu.classList.toggle(hideMenu);
+    mobileMenu.classList.toggle(showMenu);
+    if (mobileMenu.classList.contains(showMenu) && !mobileMenu.classList.contains(hideMenu)) {
+        hamburgerDiv.innerHTML = closeButton;
+        hamburgerDiv.style.transform = 'rotate(90deg)';
+    } else {
+        hamburgerDiv.innerHTML = hamburgerButton;
+        hamburgerDiv.style.transform = 'rotate(0deg)';
+    }
 }
 
 function delay(time) {
--- virtual-influencers/index.php
+++ virtual-influencers/index.php
@@ -1,20 +1,136 @@
-<!DOCTYPE html>
+<?php
+
+function getWebDetails()
+{
+
+    define("PATH", 'localhost/virtup-web/src/json/home.json');
+
+    $url = PATH;
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Accept: application/json',
+        'Content-Type: application/json',
+    ]);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    if (curl_error($ch)) {
+        return json_encode(["is_success" => false, "message_error" => "File Not Found", "response_code" => 404]);
+    } else {
+        return $response;
+    }
+}
+
+$rawRes = getWebDetails();
+$response = json_decode($rawRes);
+
+$hasAbout = false;
+
+$meta_name = "";
+$meta_description = "";
+$meta_company = "";
+$meta_url = "";
+$meta_color = "";
+
+$about_logo = "";
+$about_logo_alt = "";
+
+if (isset($response->is_success) && $response->is_success === true) {
+    $data = $response->data;
+    foreach ($data as $section) {
+        switch ($section->section) {
+            case "metadata":
+                if (isset($section->details)) {
+                    if (isset($section->details->website_name)) {
+                        $meta_name = $section->details->website_name;
+                    }
+                    if (isset($section->details->website_description)) {
+                        $meta_description = $section->details->website_description;
+                    }
+                    if (isset($section->details->website_company)) {
+                        $meta_company = $section->details->website_company;
+                    }
+                    if (isset($section->details->members_url)) {
+                        $meta_url = $section->details->members_url;
+                    }
+                    if (isset($section->details->website_color)) {
+                        $meta_color = $section->details->website_color;
+                    }
+                }
+                break;
+            case "about":
+                $hasAbout = true;
+                if (isset($section->details)) {
+                    if (isset($section->details->logo)) {
+                        if (isset($section->details->logo->path)) {
+                            $about_logo = $section->details->logo->path;
+                        }
+                        if (isset($section->details->logo->alt)) {
+                            $about_logo_alt = $section->details->logo->alt;
+                        }
+                    }
+                }
+                break;
+            default:
+        }
+    }
+}
+?>
 <html lang="en">
+
 <head>
+    <!-- SEO -->
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <meta name="title" content="<?php echo $meta_name; ?>" />
+    <meta name="application-name" content="<?php echo $meta_name; ?>" />
+    <meta name="og:site_name" content="<?php echo $meta_name; ?>" />
+    <meta name="twitter:title" content="<?php echo $meta_name; ?>" />
+    <meta property="og:title" content="<?php echo $meta_name; ?>" />
+    <meta property="og:image:alt" content="<?php echo $meta_name; ?>">
+
+    <meta property="og:type" content="website" />
+
+    <meta name="keywords" content="virtup, vtuber, streamer, entertainment" />
+
+    <meta name="author" content="<?php echo $meta_company; ?>" />
+    <meta name="description" content="<?php echo $meta_description; ?>" />
+    <meta property="og:description" content="<?php echo $meta_description; ?>" />
+    <meta name="twitter:description" content="<?php echo $meta_description; ?>" />
+
+    <meta property="og:url" content="<?php echo $meta_url; ?>">
+    <meta name="twitter:url" content="<?php echo $meta_url; ?>" />
+    <meta name="robots" content="index,follow" />
+
+    <meta property="og:image" content="https://lucian.solutions/images/22t.jpg" />
+    <meta property="og:image:secure_url" content="https://lucian.solutions/images/22t.jpg" />
+    <meta name="twitter:image" content="https://lucian.solutions/images/22t.jpg" />
+
+    <meta name="msapplication-TileColor" content="<?php echo $meta_color; ?>" />
+    <meta name="theme-color" content="<?php echo $meta_color; ?>">
+    <meta name="theme-color" media="(prefers-color-scheme: light)" content="<?php echo $meta_color; ?>" />
+    <meta name="theme-color" media="(prefers-color-scheme: dark)" content="<?php echo $meta_color; ?>" />
+
+    <title>VirtUp</title>
     <link rel="stylesheet" type="text/css" href="../src/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="../src/css/icons/font/bootstrap-icons.min.css" />
     <link rel="stylesheet" type="text/css" href="../src/css/extended.css" />
 </head>
+
 <body>
     <div class="position-fixed w-100" style="z-index:5;">
         <div class="px-md-5 px-4 d-flex flex-row justify-content-between navbar-custom">
             <a href=".." role="button">
                 <div class="d-block">
                     <div class="d-flex flex-column h-100 justify-content-center">
-                        <img src="https://lucian.solutions/images/4.png" alt="logo" class="logo" loading="lazy" />
+                        <img src="<?php echo $about_logo ?>" alt="<?php echo $about_logo_alt ?> Logo" class="logo" loading="lazy" />
                     </div>
                 </div>
             </a>
@@ -47,18 +163,19 @@
                 </div>
             </div>
             <div class="d-md-none d-block" role="button" onclick="toggleMobileNav()">
-                <div class="d-flex flex-column h-100 justify-content-center">
-                    <img src="../src/images/hamburger_menu.png" alt="menu" class="hamburger_menu" id="hamburger_menu" loading="lazy" />
+                <div class="d-flex flex-column h-100 justify-content-center" id="hamburger_menu">
+                    <i class="bi bi-list text-white display-3"></i>
                 </div>
             </div>
         </div>
         <div class="px-md-5 px-4 py-2 d-md-none hideMenu" id="mobileNav">
             <div class="d-flex flex-row justify-content-end">
                 <div class="navbar-custom rounded-3">
-                    <a href=".."role="button" class="text-white text-decoration-none" onclick="toggleMobileNav();">
+                    <a href=".." role="button" class="text-white text-decoration-none" onclick="toggleMobileNav();">
                         <div class="d-block mobile-nav-item text-end">Home</div>
                     </a>
-                    <a href="javascript:void(0)" class="text-white text-decoration-none" role="button" onclick="goToTop();toggleMobileNav();">
+                    <a href="javascript:void(0)" class="text-white text-decoration-none" role="button"
+                        onclick="goToTop();toggleMobileNav();">
                         <div class="d-block mobile-nav-item text-end">Our Virtual Influencers</div>
                     </a>
                     <a href="..#about" class="text-white text-decoration-none" onclick="toggleMobileNav();">
@@ -82,7 +199,8 @@
                 <div class="row">
                     <div class="col-md-12 my-auto">
                         <div class="d-flex flex-column justify-content-center">
-                            <h1 class="display-1 text-center pb-3 mb-3 accented" id="loading"><i class="bi bi-star-fill"></i></h1>
+                            <h1 class="display-1 text-center pb-3 mb-3 accented" id="loading"><i
+                                    class="bi bi-star-fill"></i></h1>
                             <h1 class="display-4 text-center pb-3 mb-3">Coming Soon</h1>
                         </div>
                     </div>
@@ -90,22 +208,18 @@
             </div>
         </div>
 
-        <p class="text-white text-center mt-md-5 mt-2">&copy;&nbsp;All Rights reserved by VirtUp Entertainment Co., Ltd. <small class="text-secondary">2024-<span id="presentYear">Present</span></small></p>
+        <p class="text-white text-center mt-md-5 mt-2">&copy;&nbsp;All Rights reserved by VirtUp Entertainment Co., Ltd.
+            <small class="text-secondary">2024-<?php echo date("Y"); ?></small></p>
     </div>
-    
+
 </body>
 <script type="text/javascript" src="../src/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="../src/js/jquery.min.js"></script>
 <script type="text/javascript" src="../src/js/extended.js"></script>
 <script type="text/javascript" src="../src/js/mobile_menu.js"></script>
 <script>
-    const presentYear = document.getElementById('presentYear');
-    const date = new Date();
-    const year = date.getFullYear();
-    presentYear.innerText = year;
-
-    $("#banner").ready(function() {
-        $('.fade-in-onload').each(function() {
+    $("#banner").ready(function () {
+        $('.fade-in-onload').each(function () {
             var top_of_element = $(this).offset().top;
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
@@ -117,8 +231,8 @@
         });
     });
 
-    $(window).scroll(function() {
-        $('.fade-in').each(function() {
+    $(window).scroll(function () {
+        $('.fade-in').each(function () {
             var top_of_element = $(this).offset().top;
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
@@ -130,4 +244,5 @@
         });
     });
 </script>
+
 </html>
