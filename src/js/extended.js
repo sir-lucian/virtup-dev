4a3504bc5eb7b8ec3e8132c7878398b9038e3404
Karn Kamolnavin <karn.lattecandy@gmail.com>
2025-02-18 10:49:05 +0700

    WIP

--- src/js/extended.js
+++ src/js/extended.js
@@ -55,10 +55,29 @@ async function getData(path = undefined) {
     }
 }
 
+function closeMemberInfo() {
+    const body = document.body;
+    const fullMemberInfo = document.getElementById('fullMemberInfo');
+    const card = document.getElementById('member-card');
+    const portrait = document.getElementById('member-portrait');
+    fullMemberInfo.style.opacity = '0';
+    fullMemberInfo.style.pointerEvents = 'none';
+    card.style.transform = 'translateX(-100%)';
+    card.style.opacity = '0';
+    portrait.style.opacity = '0';
+    body.style.height = 'auto';
+    body.style.overflowY = 'auto';
+}
+
 async function showMemberInfo(index) {
+    const body = document.body;
+    body.style.height = '100%';
+    body.style.overflowY = 'hidden';
+    const fullMemberInfo = document.getElementById('fullMemberInfo');
     const fullMemberInfoBody = document.getElementById('fullMemberInfoBody');
-    fullMemberInfoBody.innerHTML = `<div class="d-flex justify-content-center"><div class="spinner-border text-light" style="width: 3rem; height: 3rem;" role="status"><span class="visually-hidden">Loading...</span></div></div>`;
-    $('#fullMemberInfo').modal('show');
+    fullMemberInfoBody.innerHTML = '';
+    fullMemberInfo.style.opacity = '100%';
+    fullMemberInfo.style.pointerEvents = 'auto';
     const members = await getData('/virtup-web/src/json/members.json');
     if (members && Array.isArray(members)) {
         if (members[index] && members[index].youtube.channel_handle) {
@@ -114,6 +133,9 @@ async function showMemberInfo(index) {
                 const info = {
                     name: members[index].name,
                     handle: members[index].youtube.channel_handle,
+                    image_av:
+                        '/virtup-web/src/images/virtual-influencers/profile/' +
+                        members[index].image_profile,
                     image:
                         '/virtup-web/src/images/virtual-influencers/full/' +
                         members[index].image_full,
@@ -126,80 +148,54 @@ async function showMemberInfo(index) {
 
                 let memberHTML = '';
 
-                memberHTML += `<div class="row justify-content-center">`;
-                memberHTML += `<div class="col-lg-8">`;
-                memberHTML += `<img src="${info.image}" class="w-100" loading="lazy" alt="${info.name}">`;
+                memberHTML += `<img class="member-backdrop" src="${info.image}" id="member-blur" style="opacity: 0; transition: 0.75s ease-in-out;" />`;
+                memberHTML += `<img class="member-fullbody" src="${info.image}" id="member-portrait" style="opacity: 0; transition: 0.75s ease-in-out;" />`;
+                memberHTML += `<div class="member-profile-card d-flex flex-md-row flex-column justify-content-md-start justify-content-end w-100 h-100">`;
+                memberHTML += `<div class="d-flex flex-column justify-content-end text-start mx-md-5 mx-3" id="member-card" style="transform: translateX(-100%); opacity: 0; transition: 0.75s ease-in-out;">`;
+                memberHTML += `<div class="w-100-md pb-5 d-flex flex-column gap-2">`;
+                memberHTML += `<div>`;
+                memberHTML += `<div class="display-4 fw-bold bg-white px-2 py-1" style="width: fit-content;">${info.name}</div>`;
+                memberHTML += `</div><div>`;
+                memberHTML += `<div class="fs-4 bg-white px-2 py-1 text-secondary" style="width: fit-content;">${info.handle}</div>`;
                 memberHTML += `</div>`;
-                memberHTML += `<div class="col-lg-4">`;
-                memberHTML += `<div class="d-flex flex-column justify-content-center h-100">`;
-                memberHTML += `<div class="bg-white p-3 rounded-top d-flex justify-content-end">`;
-                memberHTML += `<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>`;
-                memberHTML += `</div>`;
-                memberHTML += `<div class="d-block px-5 pb-4 bg-white rounded-bottom">`;
-                memberHTML += `<h5 class="h3 text-nowrap">${info.name}</h5>`;
-                memberHTML += `<p class="text-secondary text-nowrap mb-1">${info.handle}</p>`;
-                if (info.subs) {
-                    memberHTML += `<p class="mb-0 text-nowrap"><i class="bi bi-people-fill me-2 text-danger"></i>${info.subs}&nbsp;<small>Subscribers</small></p>`;
+                if (info.subs || info.views) {
+                    memberHTML += `<div>`;
+                    if (info.subs) {
+                        memberHTML += `<div class="bg-white px-2 py-1" style="width: fit-content;"><i class="bi bi-people-fill me-2 text-danger"></i>${info.subs}&nbsp;<small>Subscribers</small></div>`;
+                    }
+                    if (info.views) {
+                        memberHTML += `<div class="bg-white px-2 py-1" style="width: fit-content;"><i class="bi bi-eye-fill me-2 text-danger"></i>${info.views}&nbsp;<small>Views</small></div>`;
+                    }
+                    memberHTML += `</div>`;
                 }
-                if (info.views) {
-                    memberHTML += `<p class="text-nowrap"><i class="bi bi-eye-fill me-2 text-danger"></i>${info.views}&nbsp;<small>Views</small></p>`;
-                }
-                memberHTML += `<div><a role="button" class="btn btn-outline-danger rounded fs-4 w-100" href="${info.link}" target="_blank"><i class="bi bi-youtube"></i></a></div>`;
-                memberHTML += `</div>`;
-                memberHTML += `</div>`;
-                memberHTML += `</div>`;
-                memberHTML += `</div>`;
-                memberHTML += `</div>`;
+                memberHTML += `<div>`;
+                memberHTML += `<a role="button" class="btn btn-danger fs-1 w-100" href="${info.link}" target="_blank"><i class="bi bi-youtube"></i></a></div>`;
+                memberHTML += `</div></div></div>`;
+
                 fullMemberInfoBody.innerHTML = memberHTML;
+
+                const card = document.getElementById('member-card');
+                const portrait = document.getElementById('member-portrait');
+                const blur = document.getElementById('member-blur');
+                await delay(100).then(() => {
+                    $('#member-card').ready(() => {
+                        card.style.transform = 'translateX(0)';
+                        card.style.opacity = '1';
+                        portrait.style.opacity = '1';
+                        blur.style.opacity = '0.5';
+                    });
+                });
             } else {
-                fullMemberInfoBody.innerHTML = `<div class="bg-white px-5 py-3 rounded text-nowrap text-center fw-bold"><i class="bi bi-exclamation-triangle-fill me-2 text-danger fs-5"></i>Failed to get YouTube data</div>`;
+                fullMemberInfoBody.innerHTML = `<div class="d-flex flex-column justify-content-center h-100"><div class="d-block mx-auto fw-bold"><i class="bi bi-exclamation-triangle-fill me-2 text-danger fs-5"></i>Failed to get YouTube data</div></div>`;
             }
         }
+    } else {
+        fullMemberInfoBody.innerHTML = `<div class="d-flex flex-column justify-content-center h-100"><div class="d-block mx-auto fw-bold"><i class="bi bi-exclamation-triangle-fill me-2 text-danger fs-5"></i>Failed to get YouTube data</div></div>`;
     }
 }
 
-function scrollToNextSection(id) {
-    if (
-        window.innerHeight + Math.round(window.scrollY) >=
-        document.body.offsetHeight
-    ) {
-        goToTop();
-        document.getElementById(
-            'nextSectionIs'
-        ).innerHTML = `<i class="bi bi-chevron-down"></i>`;
-    } else {
-        const sections = [
-            'top-section',
-            'virtual-influencers-section',
-            'about-section',
-            'contact-section',
-        ];
-        const targetedIndex =
-            sections.findIndex((section) => id === section) + 1;
-        if (targetedIndex < sections.length && targetedIndex >= 0) {
-            const element = document.getElementById(sections[targetedIndex]);
-            element.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start',
-                inline: 'nearest',
-            });
-        }
-    }
-}
-
-function checkScrollPosition() {
-    if (
-        window.innerHeight + Math.round(window.scrollY) >=
-        document.body.offsetHeight
-    ) {
-        document.getElementById(
-            'nextSectionIs'
-        ).innerHTML = `<i class="bi bi-chevron-up"></i>`;
-    } else {
-        document.getElementById(
-            'nextSectionIs'
-        ).innerHTML = `<i class="bi bi-chevron-down"></i>`;
-    }
+function delay(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
 }
 
 async function loadMembers() {
@@ -218,10 +214,10 @@ async function loadMembers() {
         let indexMember = 0;
         if (Array.isArray(members)) {
             memberHTML +=
-                '<div class="d-flex flex-row justify-content-center">';
+                `<div class="d-flex flex-row justify-content-center">`;
             memberHTML +=
-                '<h1 class="display-6 pb-3 mb-3 header text-center" id="virtual-influencers-title">Our Virtual Influencers</h1>';
-            memberHTML += '</div>';
+                `<h1 class="display-6 pb-3 mb-3 header text-center" id="virtual-influencers-title">Our Virtual Influencers</h1>`;
+            memberHTML += `</div>`;
             let member = undefined;
             for (member of members) {
                 if (!group_codes.includes(member.group_code)) {
@@ -238,18 +234,20 @@ async function loadMembers() {
                 memberHTML += `<div class="row justify-content-center pt-4">`;
                 for (member of members) {
                     if (member.group_code === group.group_code) {
-                        memberHTML += `<div class="col-lg-2 col-md-4 col-sm-6 px-2 d-flex flex-column justify-content-start"><img src="/virtup-web/src/images/virtual-influencers/profile/${member.image_profile}" class="d-block rounded-pill member-photo" role="button" style="border: solid 2px ${member.color}" alt="${member.name}" onclick="showMemberInfo(${indexMember});" id="member-btn-${indexMember}"} loading="lazy" /><div class="d-block text-center mt-4"><h4 class="h5">${member.name}</h4><p class="text-secondary">${member.youtube.channel_handle}</p></div></div>`;
+                        memberHTML += `<div class="col-lg-2 col-md-4 col-sm-6 px-2 my-3 d-flex flex-column justify-content-start gap-2"><img src="/virtup-web/src/images/virtual-influencers/profile/${member.image_profile}" class="d-block member-photo" role="button" style="border: solid 2px ${member.color}" alt="${member.name}" onclick="showMemberInfo(${indexMember});" id="member-btn-${indexMember}"} /><div class="d-block text-center"><div class="fw-bold fs-5">${member.name}</div><div class="text-secondary" style="font-size: 1rem;">${member.youtube.channel_handle}</div></div></div>`;
                         indexMember++;
                     }
                 }
-                memberHTML += '</div>';
-                memberHTML += '</div>';
+                memberHTML += `</div>`;
+                memberHTML += `</div>`;
                 indexGroup++;
                 if (indexGroup === groups.length - 1) {
-                    memberHTML += '<hr class="w-50 mx-auto my-5" />';
+                    memberHTML += `<hr class="w-50 mx-auto my-5" />`;
                 }
             }
         }
         membersDiv.innerHTML = memberHTML;
+        return Array.isArray(members) ? true : false;
     }
+    return false;
 }
